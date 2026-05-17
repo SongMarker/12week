6a5fb83b0874da04b2a82291e547a86a8a8215b8
Song Inseok <thddlstjrchlrh@gmail.com>
2026-05-17 18:16:58 +0900

    입력

--- main.java
+++ main.java
@@ -1,5 +1,20 @@
-package 12week;
+import java.util.Scanner;
 
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();       // 전체 사람 수
+        int a = sc.nextInt();       // 촌수 구할 사람 1
+        int b = sc.nextInt();       // 촌수 구할 사람 2
+        int m = sc.nextInt();       // 관계 수
+
+        int[][] relations = new int[m][2];
+        for (int i = 0; i < m; i++) {
+            relations[i][0] = sc.nextInt(); // 부모
+            relations[i][1] = sc.nextInt(); // 자식
+        }
+
+        System.out.println("n=" + n + " a=" + a + " b=" + b + " m=" + m);
+    }
 }
